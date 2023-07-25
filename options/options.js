ea94a777a6b259f1da4511dff2f3345379326984
Bravo Bayless <bravo@secureccs.com>
2023-07-25 15:55:30 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "flare-x-42392",
-  url: "https://flare-x-42392.botics.co",
-  api: "https://flare-x-42392.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "flare-x-42392",
+  "url": "https://flare-x-42392.botics.co",
+  "api": "https://flare-x-42392.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI8"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
